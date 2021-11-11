9e00a43749df380d8d2fa5bbd43ac093ae6eb7a6
matt <matt.adam.wisnowski@gmail.com>
2021-11-11 12:49:10 -0800

    3rd

---
+++ back-end-engineer-path/javascript/dog-years-project.js
@@ -0,0 +1,13 @@
+const myAge = XX;
+//I am XX years old - changed for privacy reasons
+var earlyYears = 2;
+//This may change later
+earlyYears = earlyYears * 10.5
+var laterYears = myAge - 2
+//This is my age minus 2
+laterYears = laterYears * 4
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears = earlyYears + laterYears
+myName = 'Matt'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`);
