b6e3380e22a4d882cc11abac5681c916db3ec999
matt <matt.adam.wisnowski@gmail.com>
2021-11-21 18:05:12 -0800

    query api done

--- back-end-engineer-path/javascript/quote-api-starting/server.js
+++ back-end-engineer-path/javascript/quote-api-starting/server.js
@@ -8,3 +8,41 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+// setup get /api/quotes/random route
+app.get('/api/quotes/random', (req, res, next) => {
+  res.send ({quote: getRandomElement(quotes)});
+});
+
+// setup get /api/quotes router
+app.get('/api/quotes', (req, res, next) => {
+  if(!req.query.person) {
+    res.send({ quotes: quotes });
+    return;
+  }
+
+  const personQuotes = quotes
+    .filter(quote => quote.person === req.query.person);
+
+  res.send({quotes: personQuotes});
+});
+
+// setup post /api/quotes route
+app.post('/api/quotes', (req, res, next) => {
+  if (!req.query.person || !req.query.quote) {
+    res.status(400).send();
+    return;
+  }
+
+  const newQuote = {
+    quote: req.query.quote,
+    person: req.query.person
+  }
+
+  quotes.push(newQuote);
+  res.status(201).send({ quote: newQuote });
+});
+
+// setup port listening
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
